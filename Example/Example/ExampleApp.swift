a2715048e40c286f8318483bddad19300f71e890
Jay Lee <k.jaylee@gmail.com>
2023-12-14 16:51:24 +0900

    FEAT: Add Example.

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by kjaylee on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
