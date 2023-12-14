ec68230d3bb3feae3f44350f12c0f443502ee6a0
Jay Lee <k.jaylee@gmail.com>
2023-12-14 16:48:11 +0900

    FEAT: Init.

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FilterOne",
+    platforms: [.iOS(.v15)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "FilterOne",
+            targets: ["FilterOne"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/MetalPetal/MetalPetal.git", .upToNextMajor(from: "1.0.0"))
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "FilterOne",
+            dependencies: ["MetalPetal"]),
+        .testTarget(
+            name: "FilterOneTests",
+            dependencies: ["FilterOne"]),
+    ]
+)
